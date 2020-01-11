d58259167f76b27a2c3cc8dafc37a67372bd808c
Backa Elias Bondpä <backa_elias.bondpa@kommunikation.lu.se>
2020-01-11 23:44:33 +0100

    adding files for github pages

---
+++ build/precache-manifest.2f4f08bf64a54e61593ad0b46b246000.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f03591f7fd7aad1a9fc4587bd675452",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "a09348a91f27c2babec8",
+    "url": "/monsters-rolodex/static/css/main.fe28e3fb.chunk.css"
+  },
+  {
+    "revision": "d499d87a993599493776",
+    "url": "/monsters-rolodex/static/js/2.1e4a2dbe.chunk.js"
+  },
+  {
+    "revision": "a09348a91f27c2babec8",
+    "url": "/monsters-rolodex/static/js/main.f7815f09.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
